4ae41e7fbc5afbcf53c9bf9691d6b2bc5e54c290
BrightIndigo <128691376+BrightIndigo@users.noreply.github.com>
2023-11-28 19:55:32 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 pygame.init()
 
+# Use consistent screen dimensions
 screen_width = 1000
 screen_height = 1000
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -10,25 +11,39 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 brick_wall = pygame.image.load("brick_wall.png")
-wall1 = pygame.Rect(0, screen_height/10, 1200, 5)
-wall2 = pygame.Rect(0, screen_height/10*2, 1200, 5)
-wall3 = pygame.Rect(0, screen_height/10*3, 1200, 5)
-wall4 = pygame.Rect(0, screen_height/10*4, 1200, 5)
-wall5 = pygame.Rect(0, screen_height/10*5, 1200, 5)
-wall6 = pygame.Rect(0, screen_height/10*6, 1200, 5)
-wall7 = pygame.Rect(0, screen_height/10*7, 1200, 5)
-wall8 = pygame.Rect(0, screen_height/10*8, 1200, 5)
-wall9 = pygame.Rect(0, screen_height/10*9, 1200, 5)
-wall10 = pygame.Rect(screen_height/10, 0, 5, 1200)
-wall11 = pygame.Rect(screen_height/10*2, 0, 5, 1200)
-wall12 = pygame.Rect(screen_height/10*3, 0, 5, 1200)
-wall13 = pygame.Rect(screen_height/10*4, 0, 5, 1200)
-wall14 = pygame.Rect(screen_height/10*5, 0, 5, 1200)
-wall15 = pygame.Rect(screen_height/10*6, 0, 5, 1200)
-wall16 = pygame.Rect(screen_height/10*7, 0, 5, 1200)
-wall17 = pygame.Rect(screen_height/10*8, 0, 5, 1200)
-wall18 = pygame.Rect(screen_height/10*9, 0, 5, 1200)
 
+# Use a consistent unit for the grid (screen_height/10)
+grid_unit = screen_height // 10
+
+border_top = pygame.Rect(0, 0, screen_width, 5)
+border_left = pygame.Rect(0, 0, 5, screen_height)
+border_right = pygame.Rect(995, 0, 5, screen_height)
+border_bottom = pygame.Rect(0, 995, screen_width, 5)
+
+wall1 = pygame.Rect(0, grid_unit, screen_width, 5)
+wall2 = pygame.Rect(0, grid_unit * 2, screen_width, 5)
+wall3 = pygame.Rect(0, grid_unit * 3, screen_width, 5)
+wall4 = pygame.Rect(0, grid_unit * 4, screen_width, 5)
+wall5 = pygame.Rect(0, grid_unit * 5, screen_width, 5)
+wall6 = pygame.Rect(0, grid_unit * 6, screen_width, 5)
+wall7 = pygame.Rect(0, grid_unit * 7, screen_width, 5)
+wall8 = pygame.Rect(0, grid_unit * 8, screen_width, 5)
+wall9 = pygame.Rect(0, grid_unit * 9, screen_width, 5)
+wall10 = pygame.Rect(grid_unit, 0, 5, screen_height)
+wall11 = pygame.Rect(grid_unit * 2, 0, 5, screen_height)
+wall12 = pygame.Rect(grid_unit * 3, 0, 5, screen_height)
+wall13 = pygame.Rect(grid_unit * 4, 0, 5, screen_height)
+wall14 = pygame.Rect(grid_unit * 5, 0, 5, screen_height)
+wall15 = pygame.Rect(grid_unit * 6, 0, 5, screen_height)
+wall16 = pygame.Rect(grid_unit * 7, 0, 5, screen_height)
+wall17 = pygame.Rect(grid_unit * 8, 0, 5, screen_height)
+wall18 = pygame.Rect(grid_unit * 9, 0, 5, screen_height)
+
+player_width = 50
+player_height = 50
+player_x = (grid_unit // 2)-(player_width/2)
+player_y = (grid_unit // 2)-(player_height/2)
+player = pygame.Rect(player_x, player_y, player_width, player_height)
 
 # Draw the brick wall once before the main loop
 screen.blit(brick_wall, (0, 0))
@@ -37,6 +52,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        player_y -= 5
+    if keys[pygame.K_DOWN]:
+        player_y += 5
+    if keys[pygame.K_LEFT]:
+        player_x -= 5
+    if keys[pygame.K_RIGHT]:
+        player_x += 5
+
+
+
+    pygame.draw.rect(screen, (200, 200, 200), border_top)
+    pygame.draw.rect(screen, (200, 200, 200), border_left)
+    pygame.draw.rect(screen, (200, 200, 200), border_right)
+    pygame.draw.rect(screen, (200, 200, 200), border_bottom)
 
     pygame.draw.rect(screen, "black", wall1)
     pygame.draw.rect(screen, "black", wall2)
@@ -56,8 +88,11 @@ while running:
     pygame.draw.rect(screen, "black", wall16)
     pygame.draw.rect(screen, "black", wall17)
     pygame.draw.rect(screen, "black", wall18)
+    pygame.draw.rect(screen, (255, 0, 0), player)
 
-    keys = pygame.key.get_pressed()
+
+
+
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000
